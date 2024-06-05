cd81a1db1149d144121ced68e8e1ba29dad97089
MaiCongSon <143474504+MaiCongSon@users.noreply.github.com>
2024-06-05 15:15:11 +0700

    6

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -66,7 +66,7 @@ namespace Test
             // Assert
             foreach (var forecast in result)
             {
-                Assert.InRange(forecast.TemperatureC, -20, 55);
+                Assert.InRange(forecast.TemperatureC, -20, 60);
             }
         }
     }
